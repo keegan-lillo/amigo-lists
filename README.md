dba3e18658f42b1dd4821aa81c261909775a1c4b
Keegan Lillo <keegan@viv.ai>
2017-12-30 16:14:40 -0800

    First

---
+++ README.md
@@ -0,0 +1,2 @@
+# Amigo Lists
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import css from './App.scss'
+
+import './reset.css'
+import './global.css'
+
+export default function App () {
+  return (
+    <div className={css.component}>
+      <span>Hello World</span>
+    </div>
+  )
+}
